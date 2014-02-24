fd0750b2616e2a09a4a0821f1939a242ff82f9a5
anstaendig <marcel.holter@gmail.com>
2014-02-24 17:55:28 +0100

    next

--- gallery_kuenstler.php
+++ gallery_kuenstler.php
@@ -171,33 +171,25 @@
                 <a href="gallery/nails04.jpg" class="fancybox-thumb" rel="nails"><img class="thumbnail" src="gallery/nails04.jpg" /></a>
                 <a href="gallery/nails05.jpg" class="fancybox-thumb" rel="nails"><img class="thumbnail" src="gallery/nails05.jpg" /></a>
             </div>
-            </br>
-            </br>
             <div class ="right_gallery">
                 <h3>.what it takes to be a hero</h3>
                 <a href="gallery/hero01.jpg" class="fancybox-thumb" rel="hero"><img class="thumbnail" src="gallery/hero01.jpg" /></a>
                 <a href="gallery/hero02.jpg" class="fancybox-thumb" rel="hero"><img class="thumbnail" src="gallery/hero02.jpg" /></a>
                 <a href="gallery/hero03.jpg" class="fancybox-thumb" rel="hero"><img class="thumbnail" src="gallery/hero03.jpg" /></a>
             </div>
-            </br>
-            </br>
             <div class="left_gallery">
                 <h3>.manipulation</h3>
                 <a href="gallery/manipulation01.jpg" class="fancybox-thumb" rel="manipulation"><img class="thumbnail" src="gallery/manipulation01.jpg" /></a>
                 <a href="gallery/manipulation02.jpg" class="fancybox-thumb" rel="manipulation"><img class="thumbnail" src="gallery/manipulation02.jpg" /></a>
                 <a href="gallery/manipulation03.jpg" class="fancybox-thumb" rel="manipulation"><img class="thumbnail" src="gallery/manipulation03.jpg" /></a>
             </div>
-            </br>
-            </br>
             <div class="right_gallery">
                 <h3>.political</h3>
                 <a href="gallery/political01.jpg" class="fancybox-thumb" rel="political"><img class="thumbnail" src="gallery/political01.jpg" /></a>
                 <a href="gallery/political02.jpg" class="fancybox-thumb" rel="political"><img class="thumbnail" src="gallery/political02.jpg" /></a>
                 <a href="gallery/political03.jpg" class="fancybox-thumb" rel="political"><img class="thumbnail" src="gallery/political03.jpg" /></a>
                 <a href="gallery/political04.jpg" class="fancybox-thumb" rel="political"><img class="thumbnail" src="gallery/political04.jpg" /></a>
-            </div>
-            </br>
-            </br>            
+            </div>            
             <div class="left_gallery">
                 <h3>.other</h3>
                 <a href="gallery/01.jpg" class="fancybox-thumb" rel="other"><img class="thumbnail" src="gallery/01.jpg" /></a>
