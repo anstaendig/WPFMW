684316716140e3da06b5fe0c66e73ef11506b9d5
Usernamenigel <nigel.frangenberg@web.de>
2014-02-25 17:42:14 +0100

    Nochmaliger Fix - Desktop PC

--- php/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $username = $_POST["loginname"];
-    $passwort = $_POST["loginpw"];
-
-    $pass = $passwort;
-    echo "Hurraaa";
-?>
--- php/user.php
+++ php/user.php
@@ -1,3 +1,9 @@
 <?php
+    session_start();
+
     echo 'Hallo '.$_SESSION['name'];
+
+    if (!isset($_SESSION['loggedin'])) {
+        header('Location: http://localhost/index.php?site=index.php');
+    }
 ?>
